89f971efeea8302ef19a1e5f0c52c9646ad3ad78
g0fher <andrew.er4455@gmail.com>
2024-11-15 23:00:08 +0100

    added js and printing

---
+++ keyboard/sctipts.js
@@ -0,0 +1,43 @@
+const display = document.getElementById('display');
+const keys = document.querySelectorAll('.key');
+
+console.log(keys);
+
+keys.forEach(key => {
+    key.addEventListener('click', () => {
+        if (display.textContent == "Print here")
+        {
+            display.textContent = "";
+        }
+
+        const keyValue = key.textContent;
+
+        if (keyValue === 'Enter') 
+        {
+            display.textContent += "\n";
+        }
+        else if (keyValue === "")
+        {
+            display.textContent += " ";
+        }
+        else if (keyValue === "Backspace")
+        {
+            display.textContent = display.textContent.slice(0, -1);
+        }
+        else if (keyValue === "↑↓")
+        {
+            if (Math.round(Math.random()))
+            {
+                display.textContent += "↑";   
+            }
+            else
+            {
+                display.textContent += "↓";
+            }
+        }
+        else
+        {
+            display.textContent += keyValue;
+        }
+    });
+});

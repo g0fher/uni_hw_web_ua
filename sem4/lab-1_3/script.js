04a09ddc22c51c9cf8b63ffbcc4943adf1b56948
g0fher <andrew.er4455@gmail.com>
2025-05-06 00:33:29 +0200

    pass 2

--- sem4/lab-1_3/script.js
+++ sem4/lab-1_3/script.js
@@ -20,20 +20,20 @@ function add_input_container_edu() {
     const title_label = document.createElement('label');
     title_label.textContent = "Title";
     const title_input = document.createElement('input');
-    title_input.id = "edu_title";
     title_input.type = 'text';
     title_input.maxLength = 50;
+    title_input.name = 'title_edu';
 
     const desc_label = document.createElement('label');
     desc_label.textContent = "Desc";
     const desc_input = document.createElement('input');
-    desc_input.id = "edu_desc";
     desc_input.type = 'text';
     desc_input.maxLength = 50;
+    desc_input.name = "desc_edu";
 
     container.appendChild(title_label);
     container.appendChild(linebrake);
-    
+
     container.appendChild(title_input);
     linebrake = document.createElement('br');
     container.appendChild(linebrake);
@@ -58,15 +58,15 @@ function add_input_container_skills() {
     title_label.textContent = "Title";
     const title_input = document.createElement('input');
     title_input.type = 'text';
-    title_input.id = "skills_title";
     title_input.maxLength = 50;
+    title_input.name = 'title_skill';
 
     const desc_label = document.createElement('label');
     desc_label.textContent = "Desc";
     const desc_input = document.createElement('input');
     desc_input.type = 'text';
-    desc_input.id = "skills_desc";
     desc_input.maxLength = 50;
+    desc_input.name = "desc_skill";
 
     container.appendChild(title_label);
     container.appendChild(linebrake);
@@ -83,3 +83,23 @@ function add_input_container_skills() {
 
     wrapper.appendChild(container);
 }
+
+function collect_edu() {
+    const titles = document.querySelectorAll('[name="title_edu"]');
+    const descs = document.querySelectorAll('[name="desc_edu"]');
+    
+    for (let i = 0; i < titles.length; i++) {
+        console.log(`Title: ${titles[i].value}, Desc: ${descs[i].value}`);
+    }
+}
+
+function collect_skills() {
+    const titles = document.querySelectorAll('[name="title_skill"]');
+    const descs = document.querySelectorAll('[name="desc_skill"]');
+    
+    for (let i = 0; i < titles.length; i++) {
+        console.log(`Title: ${titles[i].value}, Desc: ${descs[i].value}`);
+    }
+}
+
+

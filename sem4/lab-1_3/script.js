24cc3949649bfa360734dbde79eb6ed22bacbc91
g0fher <andrew.er4455@gmail.com>
2025-05-05 23:52:22 +0200

    init lab13

---
+++ sem4/lab-1_3/script.js
@@ -0,0 +1,85 @@
+// let textArea = document.getElementById("test");
+
+// function save_text() {
+//     localStorage.setItem("text", textArea.value);
+// }
+
+// function retrieve_text() {
+//     textArea.value = localStorage.getItem("text");
+// }
+
+
+
+function add_input_container_edu() {
+    const wrapper = document.getElementById("edu_wrapper");
+    let linebrake = document.createElement('br');
+    
+    const container = document.createElement('div');
+    container.classList.add("input_container");
+
+    const title_label = document.createElement('label');
+    title_label.textContent = "Title";
+    const title_input = document.createElement('input');
+    title_input.id = "edu_title";
+    title_input.type = 'text';
+    title_input.maxLength = 50;
+
+    const desc_label = document.createElement('label');
+    desc_label.textContent = "Desc";
+    const desc_input = document.createElement('input');
+    desc_input.id = "edu_desc";
+    desc_input.type = 'text';
+    desc_input.maxLength = 50;
+
+    container.appendChild(title_label);
+    container.appendChild(linebrake);
+    
+    container.appendChild(title_input);
+    linebrake = document.createElement('br');
+    container.appendChild(linebrake);
+
+    container.appendChild(desc_label);
+    linebrake = document.createElement('br');
+    container.appendChild(linebrake);
+
+    container.appendChild(desc_input);
+
+    wrapper.appendChild(container);
+}
+
+function add_input_container_skills() {
+    const wrapper = document.getElementById("skills_wrapper");
+    let linebrake = document.createElement('br');
+    
+    const container = document.createElement('div');
+    container.classList.add("input_container");
+
+    const title_label = document.createElement('label');
+    title_label.textContent = "Title";
+    const title_input = document.createElement('input');
+    title_input.type = 'text';
+    title_input.id = "skills_title";
+    title_input.maxLength = 50;
+
+    const desc_label = document.createElement('label');
+    desc_label.textContent = "Desc";
+    const desc_input = document.createElement('input');
+    desc_input.type = 'text';
+    desc_input.id = "skills_desc";
+    desc_input.maxLength = 50;
+
+    container.appendChild(title_label);
+    container.appendChild(linebrake);
+
+    container.appendChild(title_input);
+    linebrake = document.createElement('br');
+    container.appendChild(linebrake);
+    
+    container.appendChild(desc_label);
+    linebrake = document.createElement('br');
+    container.appendChild(linebrake);
+
+    container.appendChild(desc_input);
+
+    wrapper.appendChild(container);
+}
